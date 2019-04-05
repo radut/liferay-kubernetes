a0f49fe08eb7d9eb142f415149044f0b6cf6664d
ajsampang67 <ajsampang67@gmail.com>
2019-04-05 08:33:37 -0500

    Liferay, DB, Search running. S3 not fully configured

---
+++ startup.sh
@@ -0,0 +1,6 @@
+kubectl apply -f maria-master-controller.json
+kubectl apply -f maria-master-service.json
+#kubectl apply -f elasticsearch-controller.json
+#kubectl apply -f elasticsearch-service.json
+kubectl apply -f liferay-controller.json
+kubectl apply -f liferay-service.json
